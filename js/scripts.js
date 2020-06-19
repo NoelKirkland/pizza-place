830cc7855838e5286d5fa0cbbeccc91a9f1838ea
Noel Kirkland <noelkirkland@gmail.com>
2020-06-19 09:22:56 -0700

    make some edits to the my original Pizza constructor. Fill out my pizza order proto method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,14 @@
 // Buisiness Logic:
 function Pizza(topping, size){
-  this.topping = topping
-  this.size = size
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.price = price
 }
 
-Pizza.prototype.order function(pizza){
-  
+Pizza.prototype.fullOrder = function() {
+  return "You have selected a " + this.size + ", " + this.crust + " pizza with " + this.topping + " on top. Your total is $";
 }
-
-
-
-
-
-
-
-
-
 
 
 
