3803f3af384cee68c53fefd7469a3f39a6bad0b6
Noel Kirkland <noelkirkland@gmail.com>
2020-06-19 08:16:56 -0700

    create seperation labels for bl ui. Created a simple pizza constructor. created an empty prototype method.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Buisiness Logic:
+function Pizza(topping, size){
+  this.topping = topping
+  this.size = size
+}
+
+Pizza.prototype.order function(pizza){
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// User Interface:
